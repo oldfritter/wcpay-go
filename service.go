81a8784601d2a1dad99eeac22ab4889a01541b50
Leon Oldfritter <leon.zcf@gmail.com>
2020-05-15 10:44:15 +0800

    订单查询&取消订单

--- service.go
+++ service.go
@@ -11,10 +11,13 @@ import (
 
 const (
 	UnifiedOrerWCPayRequest = []string{"appid", "mch_id", "nonce_str", "sign", "body", "out_trade_no", "total_fee", "spbill_create_ip", "notify_url", "trade_type"}
+	QueryOrderWCPayRequest  = []string{"appid", "mch_id", "nonce_str", "sign", "transaction_id"}
+	CloseOrderWCPayRequest  = []string{"appid", "mch_id", "nonce_str", "sign", "out_trade_no"}
 	RefundWCPayRequest      = []string{"appid", "mch_id", "nonce_str", "sign", "transaction_id", "out_refund_no", "total_fee", "refund_fee"}
 )
 
-func UnifiedOrder(options map[string]string) (string, err error) {
+// 统一下单
+func UnifiedOrder(options map[string]string) (result UnifiedOrderResult, err error) {
 	options["appid"] = Appid
 	options["mch_id"] = MchId
 	options["nonce_str"] = nonceStr()
@@ -41,13 +44,66 @@ func UnifiedOrder(options map[string]string) (string, err error) {
 	}
 	var result UnifiedOrderResult
 	err := xmlParse(&resp.Body, &result)
-	if err != nil {
-		log.Println(err)
-		return "", err
-	}
-	return result.CodeUrl, nil
+	return result, err
 }
 
+// 订单查询
+func QueryOrder(options map[string]string) (result QueryOrderResult, err error) {
+	options["appid"] = Appid
+	options["mch_id"] = MchId
+	options["nonce_str"] = nonceStr()
+	options["sign_type"] = "MD5"
+	options["sign"] = generateSign(options)
+	err := checkRequiredOptions(options, RefundWCPayRequest)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx, cancelFun := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancelFun()
+	req, _ := http.NewRequest(
+		http.MethodPost,
+		"https://api.mch.weixin.qq.com/pay/orderquery",
+		bytes.NewReader([]byte(xmlBody(options))),
+	)
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		log.Println(err)
+		return result, err
+	}
+	err := xmlParse(&resp.Body, &result)
+	return result, err
+}
+
+// 取消订单
+func CloseOrder(options map[string]string) (result CloseOrderResult, err error) {
+	options["appid"] = Appid
+	options["mch_id"] = MchId
+	options["nonce_str"] = nonceStr()
+	options["sign_type"] = "MD5"
+	options["sign"] = generateSign(options)
+	err := checkRequiredOptions(options, CloseOrderWCPayRequest)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx, cancelFun := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancelFun()
+	req, _ := http.NewRequest(
+		http.MethodPost,
+		"https://api.mch.weixin.qq.com/pay/closeorder",
+		bytes.NewReader([]byte(xmlBody(options))),
+	)
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		log.Println(err)
+		return result, err
+	}
+	err := xmlParse(&resp.Body, &result)
+	return result, err
+}
+
+// 退款
 func Refund(options map[string]string) (result RefundResult, err error) {
 	options["appid"] = Appid
 	options["mch_id"] = MchId
--- wcpay.go
+++ wcpay.go
@@ -46,6 +46,39 @@ type RefundResult struct {
 	RefundId      string   `xml:"refund_id"`
 	RefundFee     string   `xml:"refund_fee"`
 }
+type QueryOrderResult struct {
+	XMLName       xml.Name `xml:"xml"`
+	ReturnCode    string   `xml:"return_code"`
+	ReturnMsg     string   `xml:"return_msg"`
+	Appid         string   `xml:"appid"`
+	MchId         string   `xml:"mch_id"`
+	DeviceInfo    string   `xml:"device_info"`
+	NonceStr      string   `xml:"nonce_str"`
+	Sign          string   `xml:"sign"`
+	ResultCode    string   `xml:"result_code"`
+	Openid        string   `xml:"openid"`
+	IsSubscribe   string   `xml:"is_subscribe"`
+	TradeType     string   `xml:"trade_type"`
+	BankType      string   `xml:"bank_type"`
+	TotalFee      string   `xml:"total_fee"`
+	FeeType       string   `xml:"fee_type"`
+	TransactionId string   `xml:"transaction_id"`
+	OutTradeNo    string   `xml:"out_trade_no"`
+	Attach        string   `xml:"attach"`
+	TimeEnd       string   `xml:"time_end"`
+	TradeState    string   `xml:"trade_state"`
+}
+type CloseOrderResult struct {
+	XMLName    xml.Name `xml:"xml"`
+	ReturnCode string   `xml:"return_code"`
+	ReturnMsg  string   `xml:"return_msg"`
+	Appid      string   `xml:"appid"`
+	MchId      string   `xml:"mch_id"`
+	NonceStr   string   `xml:"nonce_str"`
+	Sign       string   `xml:"sign"`
+	ResultCode string   `xml:"result_code"`
+	ResultMsg  string   `xml:"result_msg"`
+}
 
 func SetApiclientByPkcs12(pfxData *[]byte, pass string) {
 	ApiclientKey, ApiclientCert, err := pkcs12.Decode(*pfxData, pass)
